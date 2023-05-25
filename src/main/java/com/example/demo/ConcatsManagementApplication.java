67942cf45d1effb94dc1518b570f28874b3cb442
kowal319 <bartlomiej319@wp.pl>
2023-05-25 09:26:28 +0200

    message

---
+++ src/main/java/com/example/demo/ConcatsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcatsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcatsManagementApplication.class, args);
+	}
+
+}
